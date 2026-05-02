224a6458bde2b11fdf646be2e283365800a2006b
alexis <alexis@epsilver.xyz>
2026-05-02 04:37:36 -0400

    remove build dir from repo, add to .gitignore

--- build-test/CPM_modules/Findvst3sdk.cmake
+++
@@ -1,3 +0,0 @@
-include("/home/alexis/dev/silvertune/build-test/cmake/CPM_0.40.2.cmake")
-CPMAddPackage("NAME;vst3sdk;GITHUB_REPOSITORY;steinbergmedia/vst3sdk;GIT_TAG;v3.8.0_build_66;EXCLUDE_FROM_ALL;TRUE;DOWNLOAD_ONLY;TRUE;GIT_SUBMODULES;base;public.sdk;pluginterfaces;cmake;SOURCE_DIR;cpm/vst3sdk")
-set(vst3sdk_FOUND TRUE)
